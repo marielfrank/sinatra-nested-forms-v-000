7c3dae8b9b0fbac2e5bb5d6692dc6797e63f20ac
Mariel F <marielbeth@gmail.com>
2017-09-12 15:34:11 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  SHIPS = []
+  attr_reader :name, :type, :booty
+
+  def initialize(attributes)
+    @name = name
+    @type = type
+    @booty = booty
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
